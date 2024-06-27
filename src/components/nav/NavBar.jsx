a1a9b67f3c7893a830cb61c73725ca6b985c445b
C-Red20 <caila.linger@yahoo.com>
2024-06-27 10:05:41 -0400

    Trail and Error

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -1,48 +1,44 @@
-import React, { useState } from 'react'
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-} from 'reactstrap'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css'; // Import your CSS file for styling
 
 export const NavBar = () => {
-  const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(false);
 
+    const toggle = () => setIsOpen(!isOpen);
 
-  const toggle = () => setIsOpen(!isOpen);
+    return (
+        <header className="navbar-container">
+            <h1 className="navbar-title">
+                <Link to="/welcome" className="navbar-title-link">Reading Tracker</Link>
+            </h1>
+            <nav className="navbar" style={{ backgroundColor: '#70757a' }}>
+                <button className="navbar-toggler" onClick={toggle}>
+                    Menu
+                </button>
+                <div className={`navbar-collapse ${isOpen ? 'open' : ''}`}>
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a href="/books" className="nav-link">All Books</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="/reading" className="nav-link">Currently Reading</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="/finished" className="nav-link">Finished Books</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="/not-started" className="nav-link">Not Started</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="/" className="nav-link" onClick={() => localStorage.removeItem("activeUser")}>
+                                <strong>Logout</strong>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
+};
 
-  
-  return (
-    <div>
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">Reading Tracker</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={!isOpen} navbar>
-          <Nav className="me-auto" navbar>
-            <NavItem>
-              <NavLink href="/books">All Books</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/reading">Currently Reading</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/finished">Finished Books</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/not-started">Not Started</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/" onClick={() => localStorage.removeItem("activeUser")}>
-                <strong>Logout</strong>
-              </NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
-    </div>
-  )
-}
